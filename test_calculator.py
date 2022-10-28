a216794bef2c3f937f3f5a4a2ae0802e5ce89a45
Liovirgilda Mendonca <liovirgilda_developer@gmail.com>
2022-10-28 17:05:09 +0100

    test assert in a single line

--- test_calculator.py
+++ test_calculator.py
@@ -4,8 +4,11 @@ import calculator
 class TestCalculator(unittest.TestCase):
 
   def test_add(self):
+    """
     result = calculator.add(10,5)
     self.assertEqual(result,15)
+    """
+    self.assertEqual(calculator.add(10,5),15)
 
-  if __name__ == '__main__':
-    unittest.main()
+if __name__ == '__main__':
+  unittest.main()
