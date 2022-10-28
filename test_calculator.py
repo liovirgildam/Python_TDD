6712645733f864599b5de2306b592c34389c083b
Liovirgilda Mendonca <liovirgilda_developer@gmail.com>
2022-10-28 17:12:32 +0100

    Tests for subtract function

--- test_calculator.py
+++ test_calculator.py
@@ -12,7 +12,11 @@ class TestCalculator(unittest.TestCase):
     self.assertEqual(calculator.add(-1,1),0)
     self.assertEqual(calculator.add(-1,-5),-6)
     
-    
+  def test_subtract(self):
+   
+    self.assertEqual(calculator.subtract(1,-1), 2)
+    self.assertEqual(calculator.subtract(10,1),9)
+    self.assertEqual(calculator.subtract(-1,1),-2)
 
 if __name__ == '__main__':
   unittest.main()
