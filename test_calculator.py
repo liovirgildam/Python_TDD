ab5cde7269299a6e084744145223642bbaba7980
Liovirgilda Mendonca <liovirgilda_developer@gmail.com>
2022-10-28 17:37:31 +0100

    Test divide function

--- test_calculator.py
+++ test_calculator.py
@@ -18,5 +18,15 @@ class TestCalculator(unittest.TestCase):
     self.assertEqual(calculator.subtract(10,1),9)
     self.assertEqual(calculator.subtract(-1,1),-2)
 
+  def test_multiply(self):
+    self.assertEqual(calculator.multiply(1,10),10)
+    self.assertEqual(calculator.multiply(2,5), 10)
+    self.assertEqual(calculator.multiply(2,2),4)
+
+  def test_divide(self):
+    self.assertEqual(calculator.divide(4,2),2)
+    self.assertEqual(calculator.divide(10,2.5), 4)
+    self.assertEqual(calculator.divide(11,1),11)
+
 if __name__ == '__main__':
   unittest.main()
